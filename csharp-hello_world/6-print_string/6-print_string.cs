d5f7d766d0423cb5301f68056500e32c9665f204
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-01 21:58:46 -0400

    fix

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine(string.Concat(Enumerable.Repeat(str, 3)));
+		Console.WriteLine(str + str + str);
 		Console.WriteLine(str.Substring(0,9));
         }
 }
