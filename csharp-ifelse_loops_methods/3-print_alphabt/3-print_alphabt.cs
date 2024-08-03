211d1abe0e0910c034aa40284a24055816203a61
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-03 11:39:22 -0400

    print the alphabet without 'e' or 'q'

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program{
+  static void Main(string[]args)
+  {
+    string alphabet = "abcdfghijklmnoprstuvwxyz";
+    for(int i=0; i < alphabet.Length; i++){
+      Console.Write(alphabet[i]);
+    }
+  }
+}
