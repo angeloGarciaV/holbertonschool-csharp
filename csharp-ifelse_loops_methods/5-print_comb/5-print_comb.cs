0c6705dafc22203c0a1b26dd6b7f7c60a25f1bc3
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-03 16:41:41 -0400

    print numbers from 0 to 99 with commas

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[]args)
+  {
+    for(int i=0; i < 100; i++)
+    {
+      Console.Write(i.ToString("D2"));
+      if(i < 99){
+        Console.Write(", ");
+      }
+    }
+  }
+}
