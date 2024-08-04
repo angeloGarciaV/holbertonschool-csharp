1e4d1c7e1f35dbd21affdc6b0e56277987f3bb1d
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-03 22:11:28 -0400

    method to create and print an array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Array
+{
+  public static int[] CreatePrint(int size)
+  {
+    if(size == 0)
+    {
+      Console.WriteLine();
+    } else if(size < 0)
+    {
+      Console.WriteLine("Size cannot be negative");
+      return null;
+    }
+
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+      arr[i] = i;
+    }
+    foreach(int num in arr)
+    {
+      Console.Write($"{num}");
+      if(num < arr.Length-1)
+      {
+        Console.Write(" ");
+      } else{
+        Console.WriteLine();
+      }
+    }
+    return arr;
+  }
+}
---
+++ csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,9 @@
+# C# - Arrays, Lists, and Dictionaries
+
+## Project 3 -
+
+### Tasks
+
+#### 0-print_array
+
+- method that creates and prints an array of integers of a given size
