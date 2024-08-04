49c010e62d2bba96a71546fbaddbbd8552d460a8
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-04 14:31:03 -0400

    Refactor DivisibleBy2 method to handle empty lists

--- csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -5,6 +5,10 @@ class List
   public static List<bool> DivisibleBy2(List<int> myList)
   {
     List<bool> evens = new List<bool>();
+
+    if(myList.Count == 0){
+      return evens;
+    }
     for(int i = 0; i < myList.Count; i++)
     {
       if(myList[i] % 2 == 0)
