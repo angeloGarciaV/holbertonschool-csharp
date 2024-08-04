294ca6b60c149ae2a4bd6f7f5fb50d928e53c804
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-03 22:51:48 -0400

    Refactor elementAt method to fix index condition

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,12 +4,11 @@ class Array
 {
   public static int elementAt(int[] array, int index)
   {
-    if(index <= array.Length && index > 0)
+    if(index >= array.Length || index < 0)
     {
-      return array[index];
-    } else{
       Console.WriteLine("Index out of range.");
       return -1;
     }
+    return array[index];
   }
 }
