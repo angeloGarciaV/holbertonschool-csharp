2a0ac8069d5cac10da494d376dccc2328c892c84
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-04 18:14:08 -0400

    Refactor BestScore method to use Count property instead of Values.Count

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,7 +5,7 @@ class Dictionary
 {
   public static string BestScore(Dictionary<string, int> myList)
   {
-    if(myList.Values.Count == 0)
+    if(myList.Count == 0)
     {
       return "None";
     }
@@ -22,11 +22,3 @@ class Dictionary
     return person;
   }
 }
-
-
-// Class Name: Dictionary
-// Prototype: public static string BestScore(Dictionary<string, int> myList)
-// Returns the key with the biggest integer value
-// You can assume that all values are non-negative integers
-// You can assume that all students have a different score
-// If no scores are found, return “None”
