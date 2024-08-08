e6a762465b25a2f38714e2aa9c6e9fc2a0fd3cc1
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-08 00:40:02 -0400

    Refactor MyStack class to improve readability and remove unnecessary code

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,19 +5,21 @@ class MyStack
 {
   public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
   {
+    Console.WriteLine($"Number of items: {aStack.Count}");
     if(aStack.Count == 0)
     {
       Console.WriteLine("Stack is empty");
+    } else
+    {
+      Console.WriteLine($"Top item: {aStack.Peek()}");
     }
-
-    Console.WriteLine($"Number of items: {aStack.Count}");
-    Console.WriteLine($"Top item: {aStack.Peek()}");
     Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
 
-    while(aStack.Contains(search))
+    if(aStack.Contains(search))
     {
-      aStack.Pop();
+      while(aStack.Pop() != search);
     }
+
     aStack.Push(newItem);
     return aStack;
   }
