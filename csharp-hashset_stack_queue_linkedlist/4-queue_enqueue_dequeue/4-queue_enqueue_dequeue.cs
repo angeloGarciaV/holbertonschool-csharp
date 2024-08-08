c01dd31773cd8ca921f2e4b9584cdca0fcfc03ea
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-08 01:17:28 -0400

    update enqueue method

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -15,18 +15,13 @@ class MyQueue
       Console.WriteLine($"First item: {aQueue.Peek()}");
     }
     aQueue.Enqueue(newItem);
+    
+    Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+    
     if(aQueue.Contains(search))
     {
-      Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
       while(aQueue.Dequeue() != search);
     }
     return aQueue;
   }
 }
-
-// Add a new given item newItem to aQueue
-// Print if aQueue contains a given item search
-// Format: Queue contains <search>: <True / False>
-// If aQueue contains the given item search, remove all items up to and including search; otherwise, leave aQueue as is
-// You can use .Dequeue() only once
-// Return aQueue
