2f700d57c0b364e60f48283056820249732c0019
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-08 13:24:41 -0400

    Update Insert method to correctly insert a new node in an ordered LinkedList

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,15 +5,24 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> newNode = myLList.First, last = null;
-
-        while (newNode != null)
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        if (myLList.First == null)
         {
-            if (newNode.Value >= n && last.Value < n)
-                return myLList.AddBefore(newNode, n);
-            last = newNode;
-            newNode = newNode.Next;
+            myLList.AddFirst(newNode);
+            return newNode;
         }
-        return myLList.AddLast(n);
+        LinkedListNode<int> currentNode = myLList.First;
+        while (currentNode != null)
+        {
+            if (n < currentNode.Value)
+            {
+                myLList.AddBefore(currentNode, newNode);
+                return newNode;
+            }
+            currentNode = currentNode.Next;
+        }
+
+        myLList.AddLast(newNode);
+        return newNode;
     }
 }
