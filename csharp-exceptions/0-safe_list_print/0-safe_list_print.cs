a5e97f3695d5bb5e4d92a0f6bf649051a20358f4
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-19 11:23:15 -0400

    SafePrint method to handle exceptions and return the number of elements actually printed

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+  public static int SafePrint(List<int> myList, int n)
+  {
+    int num = 0;
+    try
+    {
+      for (int i = 0; i < n; i++)
+      {
+        Console.WriteLine(myList[i]);
+        num++;
+      }
+    }
+    catch (Exception e)
+    {
+      return num;
+    }
+    return num;
+  }
+}
+
+
+// Class Name: List
+// Prototype: public static int SafePrint(List<int> myList, int n)
+// n represents the number of elements to print
+// n can be bigger than the length of myList
+// Returns number of elements actually printed
+// You have to use try / catch
---
+++ csharp-exceptions/README.md
@@ -0,0 +1,25 @@
+# C# - Exceptions
+
+## Project 5 -
+
+### Tasks
+
+#### 0-safe_list_print
+
+- method that prints n elements of a list
+
+#### 1-divide_print
+
+- function that divides 2 integers and prints the result
+
+#### 2-divide_lists
+
+- function that divides element by element 2 lists
+
+#### 3-throw_exception
+
+- function that throws an Exception
+
+#### 4-throw_exception_msg
+
+- method that throws an Exception with a message
