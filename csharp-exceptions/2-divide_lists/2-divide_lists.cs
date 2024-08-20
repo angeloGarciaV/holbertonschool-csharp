75cfd1de9453790ac31570b01de23635863e9ec8
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-20 11:00:18 -0400

    Add Divide method to List class for dividing two lists element by element

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+  public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+  {
+    int i = 0;
+    List<int> results = new List<int>();
+
+    while( i < listLength)
+    {
+      try
+      {
+        results.Add( list1[i] / list2[i] );
+      }
+      catch (ArgumentOutOfRangeException)
+      {
+        Console.WriteLine("Out of range");
+      }
+      catch(DivideByZeroException)
+      {
+        Console.WriteLine("Cannot divide by zero");
+        results.Add(0);
+      }
+      i++;
+    }
+    return results;
+  }
+}
+// Write a function that divides element by element 2 lists.
+
+// Class Name: List
+// Prototype: public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+// length can be bigger than the length of either list
+// Returns a new list of all division results
+// If 2 elements can’t be divided, the division result should be equal to 0
+// If the division can’t be done (dividing by 0):
+// print Cannot divide by zero
+// If either list is too short:
+// print Out of range
+// You have to use try / catch
