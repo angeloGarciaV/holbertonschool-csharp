3f69b87849cae5abf0fd6257a4521361aefccf84
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-26 19:03:28 -0400

    Override ToString method to print Zombie's attributes

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Enemies
+{
+  /// <summary>Represents zombie Class</summary>
+  public class Zombie
+  {
+    private int health;
+    private string name = "(No name)";
+
+    /// <summary>Initializes zombie instnace</summary>
+    public Zombie()
+    {
+      health = 0;
+    }
+
+    /// <summary>Instnace of Zombie with value parameter.</summary>
+    public Zombie(int value)
+    {
+      if(value >= 0)
+      {
+        health = value;
+      }
+      else{
+        throw new ArgumentException("Health must be greater than or equal to 0");
+      }
+    }
+    /// <summary>
+    /// Gets the value of the health property
+    /// </summary>
+    /// <returns>health</returns>
+    public int GetHealth()
+    {
+      return health;
+    }
+
+    /// <summary>
+    /// Name Getter and Setter
+    /// </summary>
+    /// <value></value>
+    public string Name
+    {
+      get {return name;}
+      set {name = value;}
+    }
+    /// <summary>
+    /// Overrides ToString
+    /// </summary>
+    /// <returns>Zombie Name and health</returns>
+    public override string ToString()
+    {
+      return $"Zombie Name: {this.Name} / Total Health: {GetHealth()}";
+    }
+  }
+}
