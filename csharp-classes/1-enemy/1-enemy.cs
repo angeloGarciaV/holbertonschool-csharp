4e7c4b6e66bae9012101522b7dc7680d19e47e8c
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-26 11:41:37 -0400

    Refactor Zombie class to add health parameter and improve XML documentation

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -1,9 +1,12 @@
 ﻿namespace Enemies
 {
+  /// <summary>Represents zombie Class</summary>
   public class Zombie
   {
+    /// <summary>Represents zombie health</summary>
     public int health;
 
+    /// <summary>Initializes zombie instnace</summary>
     public Zombie()
     {
       health = 0;
