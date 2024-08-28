da6e6e3ea119310db49296cfceea435d797cacc7
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-28 15:30:31 -0400

    Added method to divide a matrix

---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyMath
+{
+  public class Matrix
+  {
+    public static int[,] Divide(int[,] matrix, int num)
+    {
+      try{
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+          for(int j = 0; j < matrix.GetLength(1); j++)
+          {
+            matrix[i, j] = matrix[i, j] / num;
+          }
+        }
+        return matrix;
+      }catch(System.DivideByZeroException)
+      {
+        Console.WriteLine("Num cannot be 0");
+        return null;
+      }catch(System.NullReferenceException)
+      {
+        return null;
+      }
+    }
+  } 
+}
