f66ad0cf96e67da4a82f89a4258c415257cf894d
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-29 17:10:40 -0400

    Update Max method to use Math.Max for finding the maximum integer in a list

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,34 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace MyMath
 {
-  /// <summary>
-  /// Represents a class that contains operations for integers.
-  /// </summary>
-  public class Operations
-  {
     /// <summary>
-    /// Returns the maximum integer from a list of integers.
+    /// Represents a class that contains operations for integers.
     /// </summary>
-    /// <param name="nums">List</param>
-    /// <returns>int</returns>
-    public static int Max(List<int> nums)
+    public class Operations
     {
-      if (nums == null || nums.Count <= 0)
+        /// <summary>
+        /// Returns the maximum integer from a list of integers.
+       /// </summary>
+        /// <param name="nums">List</param>
+        /// <returns>int</returns>
+        public static int Max(List<int> nums)
+        {
+            // If list is null or empty, return 0
+            if (nums == null || nums.Count <= 0)
             {
                 return 0;
             }
-        int max = nums[0];
-        foreach(int num in nums)
-        {
-          if(num > max)
-          {
-            max = Math.Max(max, num);
-          }
+            int max = nums[0];
+
+            foreach (var value in nums)
+            {
+                max = Math.Max(max, value);
+            }
+            return max;
         }
-        return max;
-      }
     }
-  }
+}
