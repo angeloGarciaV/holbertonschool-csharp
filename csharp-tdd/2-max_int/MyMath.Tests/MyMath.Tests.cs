2954b3f1098926789ef0ccf2b9b5ae2fac94c32d
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-29 16:08:56 -0400

    refactor tests

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -6,51 +6,39 @@ namespace MyMath.Tests
     [TestFixture]
     public class MaxIntTests
     {
-        private List<int> nums;
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public void Max_WhenListOfSeveralIntsPassed_ReturnsMaxInt(
+            [Values(-99, 0, 44, 1066)] int a,
+            [Values(-6666, 34, 1, 678932)] int b)
         {
-            nums = new List<int>();
+            List<int> nums = new List<int>() {99, 0, a, -45, 3, 22, -1106, b};
+            var expectedMax = nums.Max();
+            var result = Operations.Max(nums);
+            Assert.That(result == expectedMax);
+        }
+        
+        [Test]
+        public void Max_WhenListOfOneIntPassed_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int>() {-45};
+            var expectedMax = nums.Max();
+            var result = Operations.Max(nums);
+            Assert.That(result == expectedMax);
         }
 
         [Test]
-        public void Max_EmptyList_ReturnsZero()
+        public void Max_WhenEmptyListIsPassed_Returns0()
         {
             List<int> nums = new List<int>();
-            int result = Operations.Max(nums);
-            Assert.That(result, Is.EqualTo(0));
+            var result = Operations.Max(nums);
+            Assert.That(result == 0);
         }
 
         [Test]
-        public void Max_SingleElementList_ReturnsElement()
+        public void Max_WhenNullIsPassed_Returns0()
         {
-            List<int> nums = new List<int> { 5 };
-            int result = Operations.Max(nums);
-            Assert.That(result, Is.EqualTo(5));
-        }
-
-        [Test]
-        public void Max_MultipleElementsList_ReturnsMax()
-        {
-            List<int> nums = new List<int> { 1, 3, 7, 2, 5 };
-            int result = Operations.Max(nums);
-            Assert.That(result, Is.EqualTo(7));
-        }
-
-        [Test]
-        public void Max_NegativeNumbersList_ReturnsMax()
-        {
-            List<int> nums = new List<int> { -1, -3, -7, -2, -5 };
-            int result = Operations.Max(nums);
-            Assert.That(result, Is.EqualTo(-1));
-        }
-
-        [Test]
-        public void Max_DuplicateMaxValuesList_ReturnsMax()
-        {
-            List<int> nums = new List<int> { 1, 3, 7, 7, 5 };
-            int result = Operations.Max(nums);
-            Assert.That(result, Is.EqualTo(7));
+            var result = Operations.Max(null);
+            Assert.That(result == 0);
         }
     }
 }
