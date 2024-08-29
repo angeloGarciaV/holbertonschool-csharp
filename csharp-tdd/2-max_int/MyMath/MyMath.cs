5aa85e3adcc18d34cd555737d5076568d11c2e83
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-29 16:12:39 -0400

    Added XML comments

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -3,8 +3,16 @@ using System.Globalization;
 
 namespace MyMath
 {
+  /// <summary>
+  /// Represents a class that contains operations for integers.
+  /// </summary>
   public class Operations
   {
+    /// <summary>
+    /// Returns the maximum integer from a list of integers.
+    /// </summary>
+    /// <param name="nums">List</param>
+    /// <returns>int</returns>
     public static int Max(List<int> nums)
     {
       if (nums.Count == 0)
