a6434cda32caf77280681aa8e969ef9f59937d0a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-08-31 16:31:31 -0400

    refactor palindrome check logic and optimize code

--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -15,19 +15,24 @@ namespace Text
     /// <returns>boolean</returns>
     public static bool IsPalindrome(string s)
     {
-      if (s.Length == 0)
-      {
-        return true;
-      }
       s = s.ToLower();
-      for(int i = 0; i < s.Length / 2; i++)
+
+      List<char> sParsed = new List<char>();
+
+      for (int i = 0; i < s.Length; i++)
       {
-        if (s[i] != s[s.Length - i - 1])
+        if (s[i] >= 'a' && s[i] <= 'z')
         {
-          return false;
+          sParsed.Add(s[i]);
         }
       }
-      return true;
+      s = new string(sParsed.ToArray());
+      
+      char[] sCharArray = s.ToCharArray();
+      Array.Reverse(sCharArray);
+      string sReverse = new string(sCharArray);
+      
+      return s == sReverse;
     }
   }
 }
