ed724b5f3a5e804009347630d91f1bc0c614f2ba
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-02 22:54:10 -0400

    refactor IsOfTypeInt method

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -8,10 +8,8 @@ class Obj
   /// <summary>
   /// Checks if the object is of type int
   /// </summary>
-  /// <param name="obj">object</param>
-  /// <returns>bool</returns>
   public static bool IsOfTypeInt(object obj)
   {
-    return (obj is int);
+    return obj is int;
   }
 }
