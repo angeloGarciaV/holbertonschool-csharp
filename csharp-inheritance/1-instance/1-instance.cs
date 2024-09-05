57f6237a1167d23eb276811109ff2826cd6003fd
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-05 12:15:37 -0400

    IsInstanceOfArray method and add XML documentation

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Represents class Obj
+/// </summary>
+public class Obj
+{
+  /// <summary>
+  ///  Check if an object is an instance of an array
+  /// </summary>
+  /// <param name="obj">object</param>
+  /// <returns>bool</returns>
+  public static bool IsInstanceOfArray(object obj)
+  {
+    if(obj is Array || obj.GetType() == typeof(Array))
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
