4f0b458c1ea0bf9df47aeb050e31760af9360b51
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-05 18:47:39 -0400

    Print method to display properties and methods of a class

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>
+///  Represents an object class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method that prints the properties and methods of a class.
+    /// </summary>
+    /// <param name="myObj"></param>
+    public static void Print(object myObj)
+    {
+        Type myType = myObj.GetType();
+        PropertyInfo[] properties = myType.GetProperties();
+        MethodInfo[] methods = myType.GetMethods();
+        
+        Console.WriteLine("{0} Properties:", myType.Name);
+
+        foreach (var property in properties)
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", myType.Name);
+
+        foreach (var method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
