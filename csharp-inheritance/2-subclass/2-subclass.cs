bf5369a864b2eb8b26101db97551ddb83baa0259
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-05 18:14:34 -0400

    Add subclass example and method to check if a type is a subclass

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+/// <summary>
+/// Represents an object.
+/// </summary>
+public class Obj
+{
+  /// <summary>
+  /// Determines if a type is only a subclass of another type.
+  /// </summary>
+  /// <param name="derivedType"></param>
+  /// <param name="baseType"></param>
+  /// <returns>bool</returns>
+  public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+  {
+    return derivedType != baseType && derivedType.IsSubclassOf(baseType);
+  }
+}
