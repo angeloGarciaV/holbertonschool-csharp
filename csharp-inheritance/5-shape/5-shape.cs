b0c168e630184852ce1b3f4f16ad01408f78238a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-06 00:30:31 -0400

    Add Shape class to calculate area

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/// <summary>
+/// Represents a shape.
+/// </summary>
+class Shape
+{
+  /// <summary>
+  /// Calculates the area of the shape.
+  /// </summary>
+  /// <returns>int</returns>
+  public virtual int Area()
+  {
+    throw new NotImplementedException("Area() is not implemented");
+  }
+}
