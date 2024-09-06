c6ae9cc5c40867a262a6f671adac197b7dde18a8
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-06 00:49:28 -0400

    Add Rectangle class and inheritance example

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/// <summary>
+/// Represents a shape.
+/// </summary>
+class Shape
+{
+  /// <summary>
+  /// Calculates the area of the shape.
+  /// </summary>
+  /// <returns>int</returns>
+  public virtual int Area()
+  {
+    throw new NotImplementedException("Area() is not implemented");
+  }
+}
+
+/// <summary>
+/// Represents a Rectangle.
+/// </summary>
+class Rectangle : Shape
+{
+  private int width;
+  private int height;
+
+  public int Width
+    {
+      get {return width;}
+      set {
+        if (value < 0)
+        throw new ArgumentException("Width must be greater than or equal to 0");
+
+        width = value;
+      }
+    }
+  public int Height
+    {
+      get {return height;}
+      set {
+        if (value < 0)
+        throw new ArgumentException("Height must be greater than or equal to 0");
+
+        height = value;
+      }
+    }
+}
