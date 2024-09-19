1e976e8177d6c2ca38f22b95f743c802f311950a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-18 20:46:29 -0400

    Refactor vector addition implementation

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -14,18 +14,12 @@ class VectorMath
   /// <returns>added array of doubles</returns>
   public static double[] Add(double[] vector1, double[] vector2)
   {
-    double[] vectorSum = new double[vector1.Length];
-
-    if(vector1.Length != vector2.Length)
-      return new double[] {-1};
-
-    if(vector1.Length < 2 || vector2.Length < 2)
-      return new double[] {-1};
-
-    for(int i = 0; i < vector1.Length; i++)
-    {
-      vectorSum[i] = vector1[i] + vector2[i];
-    }
-    return vectorSum;
+    if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
+        {
+            for (int i = 0; i < vector1.Length; i++)
+                vector1[i] += vector2[i];
+            return vector1;
+        }
+        return new double[] {-1};
   }
 }
