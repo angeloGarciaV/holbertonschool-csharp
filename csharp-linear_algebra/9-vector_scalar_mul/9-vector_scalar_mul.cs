6f22f3db553ed0bcf4b41d80d6a7a0f0e7c65119
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-18 22:38:42 -0400

    Implement scalar multiplication for vectors

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Represents a class of vector math
+/// </summary>
+class VectorMath
+{
+  /// <summary>
+  ///  Multiplies a vector by a scalar
+  /// </summary>
+  /// <param name="vector">vector to multiply</param>
+  /// <param name="scalar">scalar to multiply by</param>
+  /// <returns>multiplied vector</returns>
+  public static double[] Multiply(double[] vector, double scalar)
+  {
+    if(vector.Length < 2)
+      return new double[] {-1};
+
+    double[] newVector = new double[vector.Length];
+    for(int i = 0; i< vector.Length; i++)
+    {
+      newVector[i] = vector[i]*scalar;
+    }
+    return newVector;
+  }
+}
