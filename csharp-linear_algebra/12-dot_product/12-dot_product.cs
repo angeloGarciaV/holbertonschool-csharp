431e42e8503c6a71998d5ff44353f4dea999f7f6
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-20 12:06:17 -0400

    Refactor dot product implementation to handle vectors of different dimensions

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -16,6 +16,8 @@ class VectorMath
   {
     if (vector1.Length != 2 && vector1.Length != 3)
             return -1;
+    if (vector1.Length != vector2.Length)
+            return -1;
 
     double result = 0;
     for (int i = 0; i < vector1.Length; i++)
