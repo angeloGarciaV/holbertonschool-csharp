a2472a3571482a494118004a8d0059fb41670ef5
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-21 02:43:40 -0400

    Implement 2D matrix rotation

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+  public static double[,] Rotate2D(double[,] matrix, double angle)
+  {
+    double cos = Math.Cos(angle);
+    double sin = Math.Sin(angle);
+    double[,] rotation ={{cos,-sin}, {sin, cos}};
+    double[,] res = new double[2, 2];
+    
+    for(int i =0; i< 2; i++)
+      {
+        for(int j = 0; j < 2; j++)
+        {
+          for(int k = 0; k < 2; k++)
+          {
+            res[i,j] += matrix[i,k] * rotation[k,j];
+            res[i,j] = Math.Round(res[i,j], 2);
+          }
+        }
+      }
+
+    return res;
+  }
+}
