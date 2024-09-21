d81185dbe105084b3f26db5c3a6c39b1903d0ab8
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-21 16:10:02 -0400

    Update matrix multiplication and implement 2D matrix rotation

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -2,29 +2,50 @@
 
 class MatrixMath
 {
+    /// <summary>
+  /// Represents a method to multiply two matrices.
+  /// </summary>
+  /// <param name="matrix1">The first matrix to multiply.</param>
+  /// <param name="matrix2">The second matrix to multiply.</param>
+  /// <returns>The product of the two matrices.</returns>
+  public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+  {
+    int rows1 = matrix1.GetLength(0);
+    int cols1 = matrix1.GetLength(1);
+    int rows2 = matrix2.GetLength(0);
+    int cols2 = matrix2.GetLength(1);
+    double[,] result = new double[rows1, cols2]; 
+    double sum = 0;
+    if (cols1 == rows2)
+    {
+      for(int i =0; i< rows1; i++)
+      {
+        for(int j = 0; j < cols2; j++)
+        {
+          sum = 0;
+          for(int k = 0; k < cols1; k++)
+          {
+            sum += matrix1[i,k] * matrix2[k,j];
+          }
+          result[i,j] = Math.Round(sum, 2);
+        }
+      }
+      return result;
+    }
+    return new double[,] {{-1}};
+  }
+
   public static double[,] Rotate2D(double[,] matrix, double angle)
   {
     int rows = matrix.GetLength(0);
     int cols = matrix.GetLength(1);
     double cosAngle = Math.Cos(angle);
     double sinAngle = Math.Sin(angle);
-    double sum = 0;
     double[,] rotation = new double[2,2]{{cosAngle, sinAngle}, {-sinAngle, cosAngle}};
     double[,] res = new double[rows,cols];
 
-    for(int i =0; i< rows; i++)
-      {
-        for(int j = 0; j < rotation.GetLength(1); j++)
-        {
-          for(int k = 0; k < cols; k++)
-          {
-            sum += matrix[i,k] * rotation[k,j];
-            
-          }
-          res[i,j] = Math.Round(sum,2);
-          sum = 0;
-        }
-      }
+    res = Multiply(matrix, rotation);
+    
     return res;
   }
 }
