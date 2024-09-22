85ed3e442ee18f20676d45e512c8c59f0e8bce52
Angelo Garcia <angelogarcia1029@gmail.com>
2024-09-21 22:14:42 -0400

    matrix transpose implementation and update project configuration

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Represents a class of matrix operations.
+/// </summary>
+class MatrixMath
+{
+  /// <summary>
+  /// Represents a method to transpose a matrix.
+  /// </summary>
+  /// <param name="matrix">The matrix to transpose.</param>
+  /// <returns>The transposed matrix.</returns>
+  public static double[,] Transpose(double[,] matrix)
+  {
+    if (matrix.Length == 0)
+    {
+      return new double[,] {{}};
+    }
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+    double[,] result = new double[cols, rows];
+    for (int i = 0; i < rows; i++)
+    {
+      for (int j = 0; j < cols; j++)
+      {
+        result[j, i] = matrix[i, j];
+      }
+    }
+    return result;
+  }
+}
