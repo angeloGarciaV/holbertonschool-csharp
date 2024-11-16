9e7f7044a4d7e0522e3398998cabc8305c17e5a4
Angelo Garcia <angelogarcia1029@gmail.com>
2024-11-16 10:35:06 -0400

    Add XML documentation for Queue class and CheckType method

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T"></typeparam>
 class Queue<T>{
-  
+  /// <summary>
+  /// CheckType method
+  /// </summary>
+  /// <returns>Type</returns>
   public Type CheckType(){
     return typeof(T);
   }
