a7a256e2a69ab3f3b1b0f5968a75824cb6cf803f
Angelo Garcia <angelogarcia1029@gmail.com>
2024-11-16 23:15:30 -0400

    Add base class and interfaces for door interaction functionality

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+///<summary>
+/// Base class
+/// </summary>
+abstract class Base
+{
+  public string name = "";
+
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
+}
+
+/// <summary>
+/// Interactive interface
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// Breakable Interface
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// Collectable Interface
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+
+}
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
