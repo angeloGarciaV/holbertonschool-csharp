46aaf4cef7bbb7cebd8bc9719ec82e82a4021e75
Angelo Garcia <angelogarcia1029@gmail.com>
2024-11-16 22:55:56 -0400

    Update Base class initialization and enhance interface definitions for improved functionality

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -5,7 +5,7 @@
 /// </summary>
 abstract class Base {
 
-    public string name { get; set; }
+    public string name { get; set; } = string.Empty;
 
     /// <summary>
     /// 
@@ -14,20 +14,42 @@ abstract class Base {
     public override string ToString() {
         return $"{name} is a {this.GetType()}";
     }
-
-  interface IInteractive{
-    void Interact();
-  }
-  interface IBreakable{
-    int Durability{get; set;}
-    void Break();
-  }
-  interface ICollectable{
-    bool isCollected{get; set;}
-    void Collect();
-  }
 }
 
-class TestObject : Base, IInteractive, IBreakable, ICollectable{
+interface IInteractive
+{
+  void Interact();
+}
 
+interface IBreakable
+{
+  int Durability { get; set; }
+  void Break();
+}
+
+interface ICollectable
+{
+  bool isCollected { get; set; }
+  void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+  public int Durability { get; set; }
+  public bool isCollected { get; set; }
+
+  public void Interact()
+  {
+    // Do something
+  }
+
+  public void Break()
+  {
+    // Do something
+  }
+
+  public void Collect()
+  {
+    // Do something
+  }
 }
