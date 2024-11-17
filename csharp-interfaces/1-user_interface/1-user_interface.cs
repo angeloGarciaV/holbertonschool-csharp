58dcc64a4b1de55cbc5da1bc152b9e7f2043ec26
Angelo Garcia <angelogarcia1029@gmail.com>
2024-11-16 22:58:53 -0400

    Enhance documentation for Base class and interfaces, adding summaries for clarity

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 
 /// <summary>
-/// 
+/// Base class
 /// </summary>
 abstract class Base {
 
     public string name { get; set; } = string.Empty;
 
     /// <summary>
-    /// 
+    /// Override ToString method
     /// </summary>
     /// <returns></returns>
     public override string ToString() {
         return $"{name} is a {this.GetType()}";
     }
 }
-
+/// <summary>
+/// Interactive interface
+/// </summary>
 interface IInteractive
 {
   void Interact();
 }
-
+/// <summary>
+/// Breakable interface
+/// </summary>
 interface IBreakable
 {
   int Durability { get; set; }
   void Break();
 }
-
+/// <summary>
+/// Collectable interface
+/// </summary>
 interface ICollectable
 {
   bool isCollected { get; set; }
   void Collect();
 }
-
+/// <summary>
+/// TestObject class
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
   public int Durability { get; set; }
